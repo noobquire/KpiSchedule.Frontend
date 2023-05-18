b4531fdd85c4c5b0e3e5e1a772cc733ea7603520
Oleksii Lytvynov <ostrich.alexey@gmail.com>
2023-05-18 03:07:12 +0300

    update login widget

---
+++ KpiSchedule.Frontend/ViewModels/NavBarModel.cs
@@ -0,0 +1,20 @@
+﻿namespace KpiSchedule.Frontend.ViewModels
+{
+    public class NavBarModel
+    {
+        private bool displayPersonalSchedule = false;
+        public bool DisplayPersonalSchedule
+        {
+            get
+            {
+                return displayPersonalSchedule;
+            }
+            set
+            {
+                displayPersonalSchedule = value;
+                Changed?.Invoke();
+            }
+        }
+        public Action Changed { get; set; }
+    }
+}
