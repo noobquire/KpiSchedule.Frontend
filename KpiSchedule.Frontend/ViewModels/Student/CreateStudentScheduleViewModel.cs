60fb149990134d090ad4a80dfe0b4d16a9c4986b
Oleksii Lytvynov <ostrich.alexey@gmail.com>
2023-05-21 13:29:11 +0300

    Added page to create personal schedule, updated search forms

---
+++ KpiSchedule.Frontend/ViewModels/Student/CreateStudentScheduleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using KpiSchedule.Common.Entities.Base;
+
+namespace KpiSchedule.Frontend.ViewModels.Student
+{
+    public class CreateStudentScheduleViewModel
+    {
+        public List<SubjectEntity> Subjects { get; set; }
+        public List<SubjectEntity> SelectedSubjects { get; set; } = new List<SubjectEntity>();
+
+        public bool PubliclyVisible { get; set; } = false;
+
+        public CreateStudentScheduleViewModel(List<SubjectEntity> subjects)
+        {
+            Subjects = subjects;
+        }
+    }
+}
